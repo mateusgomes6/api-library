6e5dc1d2e29c9973eb087101dd3c25f3ad4858df
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-04-21 15:42:47 -0300

    conectando ao library_app

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,1 +1,12 @@
-const express = require('express');
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',    
+  password: 'Mateus12feijoada',  
+  database: 'library_app' 
+});
+
+module.exports = db.promise();
+
+
