106ab0fa0a3379b291b388cbcab812177c53d248
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-07 11:33:25 -0300

    feat: import book routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const bookRoutes = require('./routes/bookRoutes');
 
 const PORT = 3000;
+const app = express();
 
 app.use(bodyParser.json());
+app.use('/api', bookRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
