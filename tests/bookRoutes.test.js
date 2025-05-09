634e5bf117c12cabc8c0f131f88974115a1c2034
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-09 20:29:33 -0300

    feat: initial config

---
+++ tests/bookRoutes.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const express = require('express');
+const bookRoutes = require('../src/routes/bookRoutes');
+const Book = require('../src/models/Book');
+
+const app = express();
+app.use(express.json());
+app.use('/api/books', bookRoutes);
+
+jest.mock('../src/models/Book');
