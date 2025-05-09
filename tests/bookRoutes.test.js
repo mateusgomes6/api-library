6416b123a293cc760d8cb5fdc87f6820a20f0fd7
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-09 20:33:15 -0300

    feat: insert getAllBooks api successfully test

--- tests/bookRoutes.test.js
+++ tests/bookRoutes.test.js
@@ -9,6 +9,17 @@ app.use('/api/books', bookRoutes);
 
 jest.mock('../src/models/Book');
 
-describe('Book Routes - successfully', () => {});
+describe('Book Routes - successfully', () => {
+    it('should return a list of books on GET /api/books', async () => {
+        const mockBooks = [{ id: 1, titulo: 'Livro A', autor: 'Autor X', genero: 'Gênero V', ano_publicacao: 2018 }];
+        Book.getAll.mockResolvedValue(mockBooks);
+    
+        const response = await request(app).get('/api/books');
+    
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({ books: mockBooks });
+        expect(Book.getAll).toHaveBeenCalledTimes(1);
+      });
+});
 
 describe('Book Routes - failure', () => {});
