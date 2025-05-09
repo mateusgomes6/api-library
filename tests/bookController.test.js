0acfb6b5292f6146035713527f7fc522be8c3154
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-09 16:41:26 -0300

    feat: create describe for test

--- tests/bookController.test.js
+++ tests/bookController.test.js
@@ -1,3 +1,12 @@
 const bookController = require('../controllers/bookController');
 const Book = require('../models/Book');
 const httpMocks = require('node-mocks-http');
+
+beforeEach(() => {
+    jest.clearAllMocks();
+})
+
+jest.mock('../../src/models/Book');
+
+describe('Book Controller', () => {
+});
