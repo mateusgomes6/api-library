008192ee1de51db39b7923c883d99f97305956d2
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-09 20:52:23 -0300

    feat: insert post sucess api test

--- tests/bookRoutes.test.js
+++ tests/bookRoutes.test.js
@@ -32,7 +32,7 @@ describe('Book Routes - successfully', () => {
         expect(Book.getById).toHaveBeenCalledWith(1);
     });
 
-    it('should return a book on GET /api/genre/:genero if found', async () => {
+    it('should return a book on GET /api/books/genre/:genero if found', async () => {
         const mockBook = { id: 1, titulo: 'Hackeando Tudo', autor: 'Raiam Santos', genero: 'Desenvolvimento pessoal', ano_publicacao: 1988 };
         Book.getByGenre.mockResolvedValue(mockBook);
 
@@ -41,6 +41,18 @@ describe('Book Routes - successfully', () => {
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual({ book: mockBook });
         expect(Book.getByGenre).toHaveBeenCalledWith('Desenvolvimento pessoal');
+    });
+
+    it('should return a book on POST /api/books if found', async () => {
+        const newBookData = { titulo: 'Livro F', autor: 'Autor T', genero: 'Gênero W', ano_publicacao: 2019 };
+        const mockCreatedBook = ({ id: 2, ...newBookData });
+        Book.create.mockResolvedValue(mockCreatedBook);
+
+        const response = await request(app).post('/api/books').send(newBookData);
+
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toEqual({ book: mockCreatedBook });
+        expect(Book.create).toHaveBeenCalledWith(newBookData);
     });
 });
 
