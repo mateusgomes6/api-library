8afda2c05fefc47f478c0670d1be9a4f49148d5f
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-09 16:48:22 -0300

    feat: create getAllBooks test feature

--- tests/bookController.test.js
+++ tests/bookController.test.js
@@ -9,4 +9,17 @@ beforeEach(() => {
 jest.mock('../../src/models/Book');
 
 describe('Book Controller', () => {
+    it('getAllBooks should retr=urn a list of books with status 200', async () => {
+        const mockBooks = [{ id: 1, titulo: 'Livro A', autor: 'Autor Z', genero: 'Gênero D', ano_publicacão: 2013}];
+        Book.getAll.mockResolvedValue(mockBooks);
+
+        const req = httpMocks.createRequest();
+        const res = httpMocks.createResponse();
+
+        await bookController.getAllBooks(req, res);
+
+        expect(res.statusCode).toBe(200);
+        expect(res._getJSONData()).toEqual({ books: mockBooks });
+        expect(Book.getAll).toHaveBeenCalledTimes(1);
+    });
 });
