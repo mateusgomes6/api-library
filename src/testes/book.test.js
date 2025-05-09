49e702dedd5d1d3709fd1ecb1100449b354cbad2
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-09 15:51:32 -0300

    feat: insert test update feauture

--- src/testes/book.test.js
+++ src/testes/book.test.js
@@ -9,7 +9,7 @@ db.execute = jest.fn();
 
 describe('Book Model', () => {
     it('shoud call db.execute with the correct query and parameters when getAll is called', async () => {
-        const mockBooks = [{ id: 1, titulo: 'Livro A', autor: 'Autor X' }];
+        const mockBooks = [{ id: 1, titulo: 'Livro A', autor: 'Autor X', genero: 'Gênero Z', ano_publicacao: 2018 }];
         db.execute.mockResolvedValue([mockBooks]);
 
         const books = await Book.getAll();
@@ -19,7 +19,7 @@ describe('Book Model', () => {
     });
 
     it('shoud call db.execute with the correct query and parameters when getById is called', async () => {
-        const mockBook = { id: 1, titulo: 'Livro A', autor: 'Autor X' };
+        const mockBook = { id: 1, titulo: 'Livro A', autor: 'Autor X', genero: 'Gênero H', ano_publicacao: 2017 };
         db.execute.mockResolvedValue([[mockBook]]);
 
         const book = await Book.getById(1);
@@ -30,8 +30,8 @@ describe('Book Model', () => {
 
     it('shoud call db.execute with the correct query and parameters when getByGenre is called', async () => {
         const livros = [
-            { id: 1, titulo: 'Dom Casmurro', genero: 'Romance' },
-            { id: 2, titulo: 'Memórias Póstumas de Brás Cubas', genero: 'Romance' }
+            { id: 1, titulo: 'Dom Casmurro', autor: 'Machado de Assis', genero: 'Romance', ano_publicacao: 1899 },
+            { id: 2, titulo: 'Memórias Póstumas de Brás Cubas', autor:'Machado de Assis', genero: 'Romance', ano_publicacao: 1881 }
         ];
         db.execute.mockResolvedValue([livros]);
 
@@ -42,7 +42,7 @@ describe('Book Model', () => {
     });
 
     it('shoud call db.execute with the correct query and parameters when create is called', async () => {
-        const bookData = { titulo: 'Livro B', autor: 'Autor Y', genero: 'Gênero M', ano_publicacao: 20/10/2009 };
+        const bookData = { titulo: 'Livro B', autor: 'Autor Y', genero: 'Gênero M', ano_publicacao: 2009 };
         const mockInsertId = 2;
         db.execute.mockResolvedValue([{ insertId: mockInsertId }]);
 
@@ -52,6 +52,20 @@ describe('Book Model', () => {
             [bookData.titulo, bookData.autor, bookData.genero, bookData.ano_publicacao]
         );
         expect(bookId).toBe(mockInsertId);
+    });
+
+    it('shoud call db.execute with the correct query and parameters when update is called', async () => {
+        const bookId = 1;
+        const updatedBookData = { titulo: 'Livro J', autor: 'Autor F', genero: 'Gênero N', ano_publicacao: 2015 };
+        const mockAffectedRows = 1;
+        db.execute.mockResolvedValue([{ affectedRows: mockAffectedRows }]);
+
+        const affectedRows = await Book.update(bookId, updatedBookData);
+
+        expect(db.execute).toHaveBeenCalledWith('UPDATE books SET titulo = ?, autor = ?, genero = ?, ano_publicacao = ? WHERE id = ?',
+            [updatedBookData.titulo, updatedBookData.autor, updatedBookData.genero, updatedBookData.ano_publicacao, bookId]
+        );
+        expect(affectedRows).toBe(mockAffectedRows);
     });
 });
 
