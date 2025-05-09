591a706375570930f8bf5ab6b0ac52c044a4d836
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-09 10:07:13 -0300

    fix: bug from bodyParser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 const express = require('express');
-import bodyParser from 'body-parser';
 import bookRoutes from './routes/bookRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('/api', bookRoutes);
 
 app.listen(PORT, () => {
