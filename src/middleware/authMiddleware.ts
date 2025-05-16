4794b490edbd6f828e8742cd636ef0f7e1dbd862
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-16 11:40:05 -0300

    feat: add authenticate feature

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -7,3 +7,25 @@ const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET não definido no .env!");
 }
+
+interface AuthRequest extends Request {
+    userId?: number;
+}
+
+export const authenticate = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new Error('Token de autenticação não fornecido.')
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET) as { userId: number};
+        req.userId = decoded.userId;
+        next();
+    } catch (error: any) {
+        throw new Error('Token inválido.')
+    }
+};
