5b6d1e957b7cf86bed43e69610fa8a596cd85b40
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-16 11:17:30 -0300

    feat: add register feature

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,3 +7,23 @@ const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET não definido no .env!");
 }
+
+export const register = async (req: Request, res: Response) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+    throw new Error('Username, email e password são obrigatórios.');
+    }
+
+    try {
+        const existingUser = await User.findByUsername(username);
+        if (existingUser) {
+            throw new Error('Username já existe.');
+        }
+
+        const userId = await User.create(username, email, password);
+        res.status(201).json({ message: 'Usuário registrado com sucesso.', userId});
+    } catch (error: any) {
+        res.status(500).json({ error: error.message });
+    }
+}
