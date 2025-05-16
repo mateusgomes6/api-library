7171328d55d39eb941a48ca335c82d8aa6bb9b18
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-05-16 11:30:53 -0300

    feat: add login feature

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -12,7 +12,7 @@ export const register = async (req: Request, res: Response) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-    throw new Error('Username, email e password são obrigatórios.');
+      throw new Error('Username, email e password são obrigatórios.');
     }
 
     try {
@@ -26,4 +26,29 @@ export const register = async (req: Request, res: Response) => {
     } catch (error: any) {
         res.status(500).json({ error: error.message });
     }
-}
+};
+
+export const login = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        throw new Error('Username e password são obrigatórios');
+    }
+
+    try {
+        const user = await User.findByUsername(username);
+        if (!user) {
+            throw new Error('Username inválido');
+        }
+
+        const passwordValid = await User.comparePassword(password, user.password_hash);
+        if (!passwordValid) {
+            throw new Error('Senha inválida');
+        }
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '1h'});
+        res.json({ token });
+    } catch (error: any) {
+        res.status(401).json({ error: error.message });
+    }
+};
